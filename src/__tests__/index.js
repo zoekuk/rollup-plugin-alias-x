53907593c4cca09dd63ef81bd30c4129d06f0d1b
Simon Hambly <simon.hambly@unclouded.co.uk>
2017-09-05 09:29:07 +0100

    tidy comments

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -81,24 +81,6 @@ describe('plugin', () => {
 
   describe('using rollup', () => {
 
-    // test(t =>
-    //   rollup({
-    //     entry: './files/index.js',
-    //     plugins: [alias({
-    //       fancyNumber: './aliasMe',
-    //       './anotherFancyNumber': './localAliasMe',
-    //       numberFolder: './folder',
-    //       './numberFolder': './folder',
-    //     })],
-    //   }).then(stats => {
-    //     t.is(stats.modules[0].id.endsWith('/files/nonAliased.js'), true);
-    //     t.is(stats.modules[1].id.endsWith('/files/aliasMe.js'), true);
-    //     t.is(stats.modules[2].id.endsWith('/files/localAliasMe.js'), true);
-    //     t.is(stats.modules[3].id.endsWith('/files/folder/anotherNumber.js'), true);
-    //     t.is(stats.modules[4].id.endsWith('/files/index.js'), true);
-    //     t.is(stats.modules.length, 5);
-    //   })
-    
     test('works within rollup', () => {
       return rollup({
         input: path.resolve(__dirname, './files/main.js'),
